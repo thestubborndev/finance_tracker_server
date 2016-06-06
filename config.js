be405eade7b32f2b57188ce3a378059bdc2dc6e9
Fabio Berger <fabioberger1991@gmail.com>
2016-06-05 18:01:57 -0700

    Make appId an environment variable to make it clear that it needs to be replaced with your Investment Tracker base appId

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const Currencies = require('./currencies');
 const config = {
     airtableCredentials: {
         apiKey: process.env.AIRTABLE_API_KEY,
-        appId: 'appy7d4XIf7m0TDgM',
+        appId: process.env.AIRTABLE_APP_ID,
     },
     openExchangeCredentials: {
         apiKey: process.env.OPEN_EXCHANGE_API_KEY,
@@ -15,10 +15,15 @@ const config = {
     },
     // Add to this list the crypto-assets you wish to update
     // All possible assets listed in: crypto_assets.js
-    cryptoAssetsToUpdate: [Currencies.ethereum, Currencies.bitcoin],
+    cryptoAssetsToUpdate: [
+        Currencies.ethereum,
+        Currencies.bitcoin,
+    ],
     // Add to this list the fiat currencies you wish to update
     // All possible Fiat Currencies listed in: fiat_currencies.js
-    fiatCurrenciesToUpdate: [Currencies.CHF],
+    fiatCurrenciesToUpdate: [
+        Currencies.CHF,
+    ],
 };
 
 module.exports = config;
